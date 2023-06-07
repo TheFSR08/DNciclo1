6021c0e62b6a5d6662060dc6cfed4f011b79902c
TheFSR08 <62087892+TheFSR08@users.noreply.github.com>
2023-06-07 19:56:47 -0400

    mi primer commit

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+
+function PresionarAzul(){
+    document.getElementById("img1").hidden;
+}
+function presionarVerde(){
+    document.getElementById("img1").style.backgroundColor = "green";
+}
+function presionarRed(){
+    document.getElementById("img1").style.backgroundColor = "red";
+}
+function presionarAzul1(){
+    document.getElementById("img2").style.backgroundColor = "blue";
+}
+function presionarVerde1(){
+    document.getElementById("img2").style.backgroundColor = "green";
+}
+function presionarRed1(){
+    document.getElementById("img2").style.backgroundColor = "red";
+}
